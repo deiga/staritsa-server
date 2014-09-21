19d43fa6b8dde76877a3e1709aea4b13ff86e949
Timo Sand <timo.sand@eficode.com>
2014-09-21 11:38:53 +0300

    Fixed issue with order of routes

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ var users = require('./routes/users');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/../client/app/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+if(app.get('env') === 'development') {
+  app.use(express.static(path.join(__dirname, '../client')));
+  app.use(express.static(path.join(__dirname, '../client/.tmp')));
+  app.use(express.static(path.join(__dirname, '../client/app')));
+}
+app.use(express.static(path.join(__dirname, '/dist')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,11 +35,6 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(express.static(path.join(__dirname, '../client')));
-    // This covers serving up the index page
-    app.use(express.static(path.join(__dirname, '../client/.tmp')));
-    app.use(express.static(path.join(__dirname, '../client/app')));
-
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -41,10 +42,7 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
-} else if (app.get('env') === 'production') {
-  // app.use(express.static(path.join(__dirname, '/dist')));
 }
-app.use(express.static(path.join(__dirname, '/dist')));
 
 // production error handler
 // no stacktraces leaked to user
@@ -55,6 +53,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
